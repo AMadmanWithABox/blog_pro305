096819f77e9213738a4de8b93f21d53c3256816e
choffman4 <choffman@student.neumont.edu>
2024-02-27 10:03:09 -0700

    cognito

--- lambdas/user/app.py
+++ lambdas/user/app.py
@@ -1,159 +1,147 @@
-import base64
 import boto3
-from boto3.dynamodb.conditions import Key
-from boto3.dynamodb.conditions import Attr
 from os import getenv
-from uuid import uuid4
+import os
 import json
 
 region_name = getenv('APP_REGION')
-blog_user_table = boto3.resource('dynamodb', region_name=region_name).Table('BlogUser')
+cognito_client = boto3.client('cognito-idp', region_name=region_name)
+
+# Access the User Pool ID from environment variables
+USER_POOL_ID = os.getenv('USER_POOL_ID')
 
 
 def lambda_handler(event, context):
-    http_method = event["requestContext"]["http"]["method"]
+    http_method = event["requestContext"]["http"]["method"].upper()
     print(http_method)
-    # If the user is not authenticated, they can only create a user
-    if http_method == "POST" or http_method == "post":
+
+    if http_method == "POST":
         return create_user(event, context)
-
-    # grab the auth header and decode it
-    # auth_header = event["headers"]["Authorization"]
-    # if not auth_header:
-    #     return response(401, "Unauthorized")
-    # encoded_credentials = auth_header.split(' ')[1]
-    # decoded_credentials = base64.b64decode(encoded_credentials).decode('utf-8')
-
-    # Get the user's guid from the decoded credentials
-    # current_user_guid = get_user_by_username_password(decoded_credentials.split(":")[0], decoded_credentials.split(":")[1])
-    print("Received event: " + json.dumps(event, indent=2))
-    current_user_id = event['requestContext']['authorizer']['lambda']['user_id']
-    # We only need to check the remaining http methods if the user is authenticated, we can do that here once.
-    if current_user_id is None:
-        return response(401, "Unauthorized")
-
-    if http_method == "GET":
+    elif http_method == "GET":
         return get_user(event, context)
-    if http_method == "PUT":
-        return update_user(event, context, current_user_id)
-    if http_method == "DELETE":
-        return delete_user(current_user_id)
-    # else:
-    return response(400, "invalid http method")
-    # else:   # This should never happen, but it's good to have a catch-all
-    #     return response(500, "Internal Server Error")
-
-
-# Anyone can create a user, no need for authentication
-def create_user(event, context):
-    if "body" in event and event["body"] is not None:
-        event = json.loads(event["body"])
-
-    # Check if username already exists in the table
-    user = blog_user_table.scan(FilterExpression=Attr('username').eq(event["username"]))
-    if len(user["Items"]) > 0:
-        return response(400, {"error": "Username already exists"})
-
-    # Generate a new guid for the user
-    user_id = str(uuid4())
-    # Grab the username and password from the request body
-    username = event["username"]
-    password = event["password"]
-    email = event["email"]
-
-    # check if the username or password is empty
-    if username is None or password is None:
-        return response(400, {"error": "Username and password are required"})
-
-    # Check if the username is already taken???
-    blog_user_table.put_item(Item={
-        "Id": user_id,
-        "username": username,
-        "password": password,
-        "email": email
-    })
-
-    return response(200, {"user_id": user_id, "message": "User successfully created!"})
-
-
-# Grabs the current users guid with their auth credentials
-def get_user_by_username_password(username, password):
-    # find the user in the table
-    user = blog_user_table.scan(FilterExpression=Attr('username').eq(username) & Attr('password').eq(password))
-    # if the user is found, return their guid
-    if len(user["Items"]) == 1:
-        return user["Items"][0]["Id"]
-    # if the user is not found, return None
+    elif http_method == "PUT":
+        return update_user(event, context)
+    elif http_method == "DELETE":
+        return delete_user(event)
     else:
-        return None
+        return response(400, "Invalid HTTP method")
 
 
-# This endpoint is only accessible by users of the website, returns the user's username and id.
-def get_user(event, context):
-    path = event.get("pathParameters") or {}
+def create_user(event, context):
+    try:
+        # Parse the JSON body to get the username and password
+        body = json.loads(event["body"])
+        username = body["username"]
+        password = body["password"]
 
-    # Now, even if pathParameters were None, path will be an empty dict,
-    # and the 'in' checks will not raise an error
-    if "id" in path:
-        user_id = path["id"]
-        # Fetch the user data from the table. Make sure to handle potential exceptions here.
-        user_data = blog_user_table.get_item(Key={"Id": user_id})
-        # Check if the user was found
-        if "Item" not in user_data:
-            return response(404, {"error": f"User with id {user_id} not found"})
-        user = user_data["Item"]
-        # Construct a response that only includes the user_id and username
-        user_info = {
-            "user_id": user_id,
-            "username": user.get("username", "No username")  # Provide a default value in case the username is not found
+        # Call Cognito to create the user with just the username and temporary password
+        user = cognito_client.admin_create_user(
+            UserPoolId=USER_POOL_ID,
+            Username=username,
+            TemporaryPassword=password,  # Consider using secure methods for setting initial passwords
+            MessageAction='SUPPRESS'  # Optionally suppress the welcome email
+        )
+        # Extract the user's ID (sub) from the response
+        user_id = next(attr['Value'] for attr in user['User']['Attributes'] if attr['Name'] == 'sub')
+
+        return response(200, {"message": "User successfully created", "Username": username, "UserId": user_id})
+    except Exception as e:
+        return response(400, {"error": str(e)})
+
+
+def get_user(event):
+    path_parameters = event.get('pathParameters', {})
+
+    if 'username' in path_parameters:
+        # Fetch user by username
+        return get_user_by_username(path_parameters['username'])
+    else:
+        # Fetch all users
+        return get_all_users()
+
+
+def get_user_by_username(username):
+    try:
+        user = cognito_client.admin_get_user(
+            UserPoolId=USER_POOL_ID,
+            Username=username
+        )
+        # Extract required details
+        user_details = {
+            'Username': user['Username'],
+            'Attributes': user['UserAttributes']
         }
-        return response(200, user_info)
+        return response(200, user_details)
+    except Exception as e:
+        return response(400, {"error": str(e)})
 
-    elif "username" in path:
-        username = path["username"]
-        # Fetch the user data from the table. Make sure to handle potential exceptions here.
-        user_data = blog_user_table.scan(FilterExpression=Attr("username").eq(username))
-        # Check if the user was found
-        if len(user_data["Items"]) == 0:
-            return response(404, {"error": f"User with username {username} not found"})
-        user = user_data["Items"][0]
-        # Construct a response that only includes the user_id and username
-        user_info = {
-            "user_id": user["Id"],
-            "username": user.get("username", "No username")  # Provide a default value in case the username is not found
-        }
-        return response(200, user_info)
 
-    # If no path parameters are provided, return a list of all users
-    users = blog_user_table.scan()["Items"]
-    simplified_users = [{"username": user.get("username"), "Id": user.get("Id")} for user in users]
-    return response(200, simplified_users)
+def get_all_users():
+    # Use list_users to fetch all users. Note: this might need pagination handling for a large number of users.
+    try:
+        users = cognito_client.list_users(UserPoolId=USER_POOL_ID)
+        # Simplify the response to include only necessary details like Username and possibly UserAttributes
+        simplified_users = [{'Username': user['Username']} for user in users['Users']]
+        return response(200, simplified_users)
+    except Exception as e:
+        return response(400, {"error": str(e)})
 
 
 #   Only the user can update their own account, we grab their guid by get_user_by_username_password(username, password):
 def update_user(event, context, user_id):
+    # Parse the incoming event
     if "body" in event and event["body"] is not None:
-        event = json.loads(event["body"])
+        body = json.loads(event["body"])
+        username = body.get("username")
+        new_password = body.get("password")
 
-    username = event["username"]
-    password = event["password"]
+    # Check if username is provided and non-empty
+    if not username or not username.strip():
+        return response(400, "Username is required and cannot be empty")
 
-    user = blog_user_table.get_item(Key={"Id": user_id})["Item"]
+    user_attributes_update_list = []
 
-    if username is not None:
-        user['username'] = username
-    if password is not None:
-        user['password'] = password
+    # Update user attributes if any are provided
+    if user_attributes_update_list:
+        try:
+            cognito_client.admin_update_user_attributes(
+                UserPoolId=USER_POOL_ID,
+                Username=username,
+                UserAttributes=user_attributes_update_list
+            )
+        except Exception as e:
+            return response(400, {"error": str(e)})
 
-    blog_user_table.put_item(Item=user)
+    # Check if password is provided and non-empty, then update it
+    if new_password and new_password.strip():
+        try:
+            cognito_client.admin_set_user_password(
+                UserPoolId=USER_POOL_ID,
+                Username=username,
+                Password=new_password,
+                Permanent=True  # Set to False if you want the user to change the password at next sign-in
+            )
+        except Exception as e:
+            return response(400, {"error": str(e)})
 
-    return response(200, user)
+    return response(200, {"message": "User updated successfully"})
 
 
 # Only the user can delete their own account, we grab their guid by get_user_by_username_password(username, password):
-def delete_user(user_id):
-    output = blog_user_table.delete_item(Key={"Id": user_id})
-    return response(200, output)
+def delete_user(event):
+    username = event.get("pathParameters", {}).get("username")
+    if not username:
+        return response(400, "Username is required")
+
+    try:
+        cognito_client.admin_delete_user(
+            UserPoolId=USER_POOL_ID,
+            Username=username
+        )
+        return response(200, {"message": "User successfully deleted"})
+    except cognito_client.exceptions.UserNotFoundException:
+        return response(404, {"error": "User not found"})
+    except Exception as e:
+        return response(400, {"error": str(e)})
 
 
 def response(code, body):
